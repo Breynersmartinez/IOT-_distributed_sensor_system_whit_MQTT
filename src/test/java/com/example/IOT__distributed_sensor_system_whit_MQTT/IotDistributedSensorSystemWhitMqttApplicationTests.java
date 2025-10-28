58d95632c5bd4a09f1cafa5d65f23655666b3766
Breiner Saul Martinez Muñoz <153401516+Breynersmartinez@users.noreply.github.com>
2025-10-28 17:28:37 -0500

    Se cargaron los archivos del proyecto

---
+++ src/test/java/com/example/IOT__distributed_sensor_system_whit_MQTT/IotDistributedSensorSystemWhitMqttApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.IOT__distributed_sensor_system_whit_MQTT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotDistributedSensorSystemWhitMqttApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
